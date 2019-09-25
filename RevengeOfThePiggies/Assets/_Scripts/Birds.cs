b59da132b3f0b1f22752a55b56a77d7404d94933
Sharmarke <sharmarkeali239@gmail.com>
2019-09-25 14:24:24 -0400

    Final adjustments

--- RevengeOfThePiggies/Assets/_Scripts/Birds.cs
+++ RevengeOfThePiggies/Assets/_Scripts/Birds.cs
@@ -7,6 +7,7 @@ public class Birds : MonoBehaviour
 {
     public float health = 4f;
     public static int BirdCount = 0;
+    public GameObject deathEffect;
 
     private void Start()
     {
@@ -25,6 +26,7 @@ public class Birds : MonoBehaviour
     {
         Destroy(gameObject);
         BirdCount--;
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
         if(BirdCount==0)
         {
             SceneManager.LoadScene("WinScreen");
